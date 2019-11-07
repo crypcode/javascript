8eb9aa3718840488eb2f627051b0e25853325095
Exam User <exam@gmail.com>
2019-11-07 11:26:02 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,81 @@
-let suma = 1000;
-let vardas = 'Saidas';
-let darbas = 'Programuotojas';
-let pastas = 'Saidas.gum@gmail.com';
-console.log('Mano varda: '+ vardas +' Mano svajoniu darbas: '+ darbas +' Mano el. pastas yra: '+ pastas);
+
+"use strict"
+//let vardas = "Martynas";
+//let pirkiniai = ["duona", "sviestas", "druskos", "aliejus", "cesnakas"];
+/*let zmogus = {
+	akis:2,
+	kojas:2,
+	vardas:"Martynas",
+	savybes:{
+		iq:'nezinomas',
+		ugis:'2m.'
+	}
+}
+let pirkiniai = [
+	{
+		pavadinimas:'Labai gera preke',
+		kiekis:'3 vnt.',
+	},
+	{
+		pavadinimas:'Labai gera preke',
+		kiekis:'122 vnt.',
+	}
+];
+
+let kiekis = 1;
+
+console.log(kiekis ? "Preke yra" : "prekes nera") */
+/*
+var vardai = 'ieva, milda, petras';
+vardai = vardai.split(',');
+//vardai = vardai.join('+')
+vardai.push('Andriucha');
+vardai.pop();
+vardai.shift();
+vardai.unshift('Paulius');
+//vardai.splice(1,1);
+console.log(vardai); */
+
+/*let vaisiai = ['obuolys', 'bananas','kriause','braske', 'zemuoge', 'slyva', 'avietes', 'granata', 'greifurtas', 'melionas'];
+vaisiai.splice(5,1);
+vaisiai.splice(5,0,'vaisius1','vaisius2');
+console.log(vaisiai); */
+
+let vidurkis = 15;
+let lankomumas = 6;
+/*if (vidurkis == 10){
+	console.log("Puikus mokinys");
+} else if ( vidurkis <= 9){
+	console.log("geras mokinys");
+} else if (vidurkis <= 7){
+	console.log("normalus mokinys");
+}else if (vidurkis = 6){
+	console.log("patenkinamas mokinys");
+}
+	
+ else{
+	console.log("blogas mokinys");
+} */
+switch (vidurkis) {
+	case 10>:
+	  console.log("netinkamas pazymys");
+	  break;
+    case 10:
+      console.log("Puikus mokinys");
+      break;
+    case 9:
+      console.log("Puikus mokinys");
+      break;
+    case 8:
+      console.log("geras mokinys");
+      break;
+    case 7:
+      console.log("norm mokinys");
+      break;
+    case 6:
+      console.log("patenkinamas mokinys");
+      break;    
+    default:
+      console.log("Blogas mokinys");
+  }
+
