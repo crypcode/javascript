5461c4f80a722f5948e202ee6db85d89ddda7a76
Exam User <exam@gmail.com>
2019-11-07 09:10:12 +0200

    basics

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let suma = 1000;
+let vardas = 'Saidas';
+let darbas = 'Programuotojas';
+let pastas = 'Saidas.gum@gmail.com';
+console.log('Mano varda: '+ vardas +' Mano svajoniu darbas: '+ darbas +' Mano el. pastas yra: '+ pastas);
