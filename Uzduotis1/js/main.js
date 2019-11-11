4f4e3e51688f3421433d112613b14520cdb9828e
Exam User <exam@gmail.com>
2019-11-11 18:50:07 +0200

    uzduotis vienas 80%

--- Uzduotis1/js/main.js
+++ Uzduotis1/js/main.js
@@ -1,86 +1,98 @@
-
-
 let knygos = [
-{	detektyvai:[
+{	DETEKTYVAI:[
 	{
 		ISBN:'9786094791550',
-		Leidimo_metai:'2016',
+		Leidimo_metai:2016,
 		Pavadinimas:'Sausra',
 		Puslapiu_skaicius:'354 psl.'
 	},
 	{	
 		ISBN:'9786094791246',
-		Leidimo_metai:'2018',
+		Leidimo_metai:2018,
 		Pavadinimas:'Baimė',
 		Puslapiu_skaicius:'287 psl.'
 	},
 	{	
 		ISBN:'9786094790676',
-		Leidimo_metai:'2017',
+		Leidimo_metai:2017,
 		Pavadinimas:'Tamsus vanduo',
 		Puslapiu_skaicius:'521 psl.'
 	},
 	{	
 		ISBN:'9786094790683',
-		Leidimo_metai:'2019',
+		Leidimo_metai:2019,
 		Pavadinimas:'Vasarnamis su baseinu',
 		Puslapiu_skaicius:'521 psl.'
 	}
 	]
 	},
-{	asmenybes_tobulinimas:[
+{	ASMENYBES_TOBULINIMAS:[
 	{
 		ISBN:'9786098254099',
-		Leidimo_metai:'2019',
+		Leidimo_metai:2019,
 		Pavadinimas:'Neribota atmintis',
 		Puslapiu_skaicius:'789 psl.'
 	},
 	{
 		ISBN:'9786094271106',
-		Leidimo_metai:'2015',
+		Leidimo_metai:2015,
 		Pavadinimas:'Save keičiančios smegenys',
 		Puslapiu_skaicius:'315 psl.'
 	},
 	{
 		ISBN:'9786094791017',
-		Leidimo_metai:'2018',
+		Leidimo_metai:2018,
 		Pavadinimas:'Džiaugsmo knyga',
 		Puslapiu_skaicius:'454 psl.'
 	},
 	{
 		ISBN:'9786090403624',
-		Leidimo_metai:'2012',
+		Leidimo_metai:2012,
 		Pavadinimas:'Emocinės laisvės technika: revoliucinė sistema gyvenimui be streso',
 		Puslapiu_skaicius:'789 psl.'
 	}
 	]
 	},
-{	pasakos:[ 
+{	PASAKOS:[ 
 	{
 		ISBN:'9789955138501',
-		Leidimo_metai:'2019',
+		Leidimo_metai:2019,
 		Pavadinimas:'Eglė žalčių karalienė',
 		Puslapiu_skaicius:'260 psl.'
 	},
 	{
 		ISBN:'9786094403071',
-		Leidimo_metai:'2015',
+		Leidimo_metai:2015,
 		Pavadinimas:'Mano penkių minučių pasakų lobynas',
 		Puslapiu_skaicius:'186 psl.'
 	},
 	{
 		ISBN:'9786094241246',
-		Leidimo_metai:'2019',
+		Leidimo_metai:2019,
 		Pavadinimas:'Trys paršiukai',
 		Puslapiu_skaicius:'16 psl.'
 	},
 	{
 		ISBN:'9789955075523',
-		Leidimo_metai:'2014',
+		Leidimo_metai:2014,
 		Pavadinimas:'Vilkas ir septyni ožiukai',
 		Puslapiu_skaicius:'12 psl.'
 	}
 	]
 	}
 ];
-console.log(knygos);
+// for(i=0;i<knygos.length;i++){console.log(knygos[i])}; // Isskirsto knygas i kategorijas
+
+for(kategorija of knygos){
+	for(duomenys in kategorija){
+		console.log(duomenys)
+		for(knyga of kategorija[duomenys])
+			//console.log(knyga)
+				for(info in knyga)
+						console.log(info+" : "+knyga[info])
+					
+	}
+};
+
+
+//for(duomenys in kategorija){console.log(duomenys)};
