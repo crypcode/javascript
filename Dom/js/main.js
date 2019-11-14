ba723d72febb04361e3fa4af21b898dc146ed07e
Exam User <exam@gmail.com>
2019-11-14 18:27:51 +0200

    dom atn

--- Dom/js/main.js
+++ Dom/js/main.js
@@ -1,6 +1,30 @@
+//Elementu kurimas
+const button = document.querySelector('button');
+const addTask = document.querySelector('input');
+const tasks = document.querySelector('ul');
+const delTask = document.querySelector('button.button2');
+
+button.addEventListener('click', addElement);
+
+function addElement() {
+    const task = document.createElement('li');
+    task.textContent=addTask.value;
+    tasks.appendChild(task);
+}
+//Elementu deletinimas
+delTask.addEventListener('click', deleteTheTask);
+function deleteTheTask() {
+    const lasTask = document.querySelector('li:last-child');
+    tasks.removeChild(lasTask);
+}
+
+
+/*
 const button = document.querySelector('button');
 const container  = document.querySelector('.container');
 const color  = document.querySelector('input');
+const vardas  = document.querySelector('input.tekstas');
+const text = document.querySelector('p.text')
 const antraste  = document.querySelector('input.antraste');
 const pastraipa  = document.querySelector('input.pastraipa');
 const h  = document.querySelectorAll('h1');
@@ -9,6 +33,7 @@ const p  = document.querySelectorAll('p');
 button.addEventListener('click', changeColor);
 
 function changeColor() {
+    text.innerHTML = vardas.value;
     container.style.backgroundColor = color.value;
     for(item of h) {
         item.style.color = antraste.value;
@@ -17,10 +42,13 @@ function changeColor() {
         item2.style.color = pastraipa.value;
     }
 }
-
-
-
-
+*/
+/*
+const link = document.querySelectorAll('a');
+    for(linkas of link){
+        linkas.href = 'http://kitm.lt';
+    }
+*/
 
 //Fonas
 // Antrastes teksto spalva .antraste
